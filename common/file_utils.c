45bb387be28cd02c49f4114c2c7ea094dad5480e
gliontos <sdi1600094@di.uoa.gr>
2019-05-20 13:00:32 +0300

    More progress

--- common/file_utils.c
+++ common/file_utils.c
@@ -1,9 +1,25 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 #include "common_types.h"
 #include "file_utils.h"
 #include "macros.h"
+
+u64 compute_file_hash(entire_file file) {
+    u64 hash_result = 0U;
+    size_t double_words = file.size / sizeof(u64);
+    size_t remaining_bytes = file.size % sizeof(u64);
+    for (size_t i = 0U; i != double_words; ++i) {
+        hash_result += *(u64 *) file.data;
+        file.data += sizeof(u64);
+    }
+    for (size_t i = 0U; i != remaining_bytes; ++i) {
+        hash_result += *(byte *) file.data;
+        file.data += sizeof(byte);
+    }
+    return hash_result;
+}
 
 int read_line_from_stdin(void *restrict stdin_buffer, size_t bytes) {
     return read(STDIN_FILENO, stdin_buffer, bytes);
@@ -24,8 +40,39 @@ entire_file read_entire_file_into_memory(const char *filename) {
     return file;
 }
 
-void get_all_pathnames_and_versions(const char *restrict root_directory,
-                                    versioned_pathname **pathnames_out,
-                                    size_t *pathnames_n_out) {
+static void
+__add_pathname_and_version_to_list(const char *restrict pathname, list *list) {
+    versioned_pathname *vpathname = __MALLOC__(1, versioned_pathname);
+    memcpy(vpathname->pathname, pathname, MAX_PATHNAME_SIZE);
+    entire_file file = read_entire_file_into_memory(pathname);
+    if (IS_EMPTY(file)) {
+        free(vpathname);
+        return;
+    }
+    vpathname->version = compute_file_hash(file);
+    list_rpush(list, vpathname);
+}
 
+void get_all_pathnames_and_versions(const char *restrict root_directory, list *pathnames_out) {
+    char path_buffer[1024];
+    struct dirent *entry;
+    DIR *dir = opendir(root_directory);
+
+    if (!dir) return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            strcpy(path_buffer, root_directory);
+            strcat(path_buffer, "/");
+            strcat(path_buffer, entry->d_name);
+
+            if (entry->d_type == DT_REG) {
+                __add_pathname_and_version_to_list(path_buffer, pathnames_out);
+            } else {
+                get_all_pathnames_and_versions(path_buffer, pathnames_out);
+            }
+        }
+    }
+
+    closedir(dir);
 }
--- common/file_utils.h
+++ common/file_utils.h
@@ -5,11 +5,14 @@
 #include <stdbool.h>
 #include "attributes.h"
 #include "../shared_client_buffer.h"
+#include "../generic_multithreaded_list.h"
 
 typedef struct entire_file {
     void *data;
     size_t size;
 } entire_file;
+
+u64 compute_file_hash(entire_file file);
 
 __NON_NULL__(1)
 entire_file read_entire_file_into_memory(const char *filename);
@@ -17,9 +20,7 @@ entire_file read_entire_file_into_memory(const char *filename);
 __NON_NULL__(1)
 int read_line_from_stdin(void *restrict stdin_buffer, size_t bytes);
 
-__NON_NULL__(1, 2, 3)
-void get_all_pathnames_and_versions(const char *restrict root_directory,
-                                    versioned_pathname **pathnames_out,
-                                    size_t *pathnames_n_out);
+__NON_NULL__(1, 2)
+void get_all_pathnames_and_versions(const char *restrict root_directory, list *pathnames_out);
 
 #endif //EXERCISE_III_FILE_UTILS_H
